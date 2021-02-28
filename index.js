098ca28c845e4096c50c19b82b098763a58f7317
Eren Pekşen <peksen.eren@gmail.com>
2021-02-28 23:26:10 +0300

    Fixes for explicits
    Min max setters are still not working

--- index.js
+++ index.js
@@ -13,22 +13,39 @@ $("div.filter.full-span").draggable({
 });
 
 function onDragStop(event, ui){
+    console.log(event);
     $(event.target).css("width", "100%");
     $(event.target).css("inset", "auto");
 
     var trimmedStr = $.trim(event.target.innerText);
     var text = trimmedStr.substr(trimmedStr.indexOf(' ')+1);
     var firstText = trimmedStr.split(" ")[0];
-    var min = $($(event.target).find(":input.minmax")[0]).val();
-    var max = $($(event.target).find(":input.minmax")[1]).val();
+
+    if(firstText !== "pseudo"){
+        firstText = "explicit";
+        text = trimmedStr;
+    }
+
+    var min = $($(event.target).find("input")[0]).val();
+    var max = $($(event.target).find("input")[1]).val();
 
     $(event.toElement).closest(".filter-group.expanded").find(".multiselect__option span:contains('"+ text + "')").each(function(index, element)
     {
         if($(element)[0].innerText === text && $(element).parent().find("i")[0].innerText === firstText)
         {
-            $(element).trigger("click");
+            $.when($(element).trigger("click")).then(function(){
+                setTimeout(
+                    function() 
+                    {
+                        $(event.target).find("button.btn.remove-btn").trigger("click")     
+        
+                        var newAddedFilter = $(event.toElement).closest(".filter-group-body").find("div.filter.full-span").last();
+                        $(newAddedFilter.find("input")[0]).val(min);
+                        $(newAddedFilter.find("input")[1]).val(max);
+                    }, 500);
+            });
+
+            return false;
         }
     });
-
-    $(event.target).find("button.btn.remove-btn").trigger("click");
 }
